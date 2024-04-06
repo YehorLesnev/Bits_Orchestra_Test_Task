932a737ffc045f8978d89b00f42a55bb6d8071a8
Yehor Lesnevych <egor.lesnev@ukr.net>
2024-04-06 14:10:49 +0300

    Add ChangeEmployeeSalaryTypeName migration

--- Migrations/20240406103134_ChangeEmployeeSalaryTypeName.Designer.cs
+++ Migrations/20240406103134_ChangeEmployeeSalaryTypeName.Designer.cs
@@ -4,6 +4,7 @@ using Bits_Orchestra_Test_Task.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Bits_Orchestra_Test_Task.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240406103134_ChangeEmployeeSalaryTypeName")]
+    partial class ChangeEmployeeSalaryTypeName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20240406103134_ChangeEmployeeSalaryTypeName.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bits_Orchestra_Test_Task.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeEmployeeSalaryTypeName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "employee_salary",
+                table: "employees",
+                type: "Decimal(10,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "Decimal(5,2)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "employee_salary",
+                table: "employees",
+                type: "Decimal(5,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "Decimal(10,2)");
+        }
+    }
+}
