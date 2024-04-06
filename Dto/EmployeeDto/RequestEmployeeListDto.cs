2538f9b33ab425e9f1471a3f9028c269aab80501
Yehor Lesnevych <egor.lesnev@ukr.net>
2024-04-06 15:22:31 +0300

    Delete RequestEmployeeListDto.cs

--- Dto/EmployeeDto/RequestEmployeeListDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Bits_Orchestra_Test_Task.Dto.EmployeeDto
-{
-    public class RequestEmployeeListDto
-    {
-        public required IEnumerable<RequestEmployeeDto> Employees { get; set; }
-    }
-}
